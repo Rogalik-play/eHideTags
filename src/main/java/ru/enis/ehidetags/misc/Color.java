f5d2280ebc0344d8cb201dd5ab8145cacc6fcddd
en0tuk <Kolobokovich123@gmail.com>
2022-01-20 20:04:24 +0600

    I think it is last code update for 1.6

--- src/main/java/ru/enis/ehidetags/misc/Color.java
+++ src/main/java/ru/enis/ehidetags/misc/Color.java
@@ -5,18 +5,15 @@ import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 import ru.enis.ehidetags.Core;
 
 public class Color {
-    private static final LegacyComponentSerializer HexSerializer = LegacyComponentSerializer.builder().hexColors().useUnusualXRepeatedCharacterHexFormat().build();
-    private static final LegacyComponentSerializer AmpersandSerializer = LegacyComponentSerializer.builder().character('&').build();
+    private static final LegacyComponentSerializer LegacySerializer = LegacyComponentSerializer.legacyAmpersand();
+    private static final LegacyComponentSerializer HexSerializer = LegacySerializer.toBuilder().hexColors().useUnusualXRepeatedCharacterHexFormat().build();
 
     public static TextComponent ColorFormat(String translate){
         if (Core.majorMinecraftVersion() > 15) {
             return HexSerializer.deserialize(translate);
         } else {
-            return AmpersandSerializer.deserialize(translate);
+            return LegacySerializer.deserialize(translate);
 
         }
     }
-    /*
-     * Здесь будет поддержка HEX цветов
-     */
 }
