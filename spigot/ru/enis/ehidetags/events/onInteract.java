990fab3028c60eb1fd7956973048c475651a1792
en0tuk <kolobokovich123@gmail.com>
2022-11-19 16:59:08 +0600

    New Features: /eht test command Removed commit hash from version string Optimized dependencies

--- spigot/ru/enis/ehidetags/events/onInteract.java
+++ spigot/ru/enis/ehidetags/events/onInteract.java
@@ -2,11 +2,13 @@ package ru.enis.ehidetags.events;
 
 import net.kyori.adventure.text.Component;
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Tameable;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractAtEntityEvent;
+import org.jetbrains.annotations.Nullable;
 import ru.enis.ehidetags.Core;
 import ru.enis.ehidetags.misc.*;
 import net.kyori.adventure.audience.Audience;
@@ -25,21 +27,25 @@ public class onInteract implements Listener {
 
     @EventHandler
     public void interact(PlayerInteractAtEntityEvent e) {
+        returnActionBar(e.getRightClicked(), e.getPlayer());
+    }
+
+    public static void returnActionBar(Entity entity, @Nullable Player player) {
         //Проверка на игрока
-        if (e.getRightClicked() instanceof Player && !e.getRightClicked().hasMetadata("NPC")) {
+        if (entity instanceof Player && !entity.hasMetadata("NPC")) {
             //Проверка на включенный ЭкшнБар
             if (Data.enabled) {
                 //Берет игрока для Audience
-                final Audience audience = adventure().player(e.getPlayer());
+                final Audience audience = adventure().player(player);
 
                 //Берет игрока на ПКМ
-                Player rc = (Player) e.getRightClicked();
+                Player rc = (Player) entity;
 
                 //Мои заменители
                 String placeholders = Format.playerPlaceholders(Data.player_message, rc);
 
                 //Заменители из PAPI
-                placeholders = String.valueOf(new PlaceholderAPIHook(rc, placeholders, e.getPlayer()).REL);
+                placeholders = String.valueOf(new PlaceholderAPIHook(rc, placeholders, player).REL);
 
                 //Форматирование цвета
                 Component color = colorize(placeholders);
@@ -48,11 +54,11 @@ public class onInteract implements Listener {
                 audience.sendActionBar(color);
             }
         }
-        if (e.getRightClicked() instanceof Tameable && !e.getPlayer().isSneaking() && ((Tameable) e.getRightClicked()).isTamed()) {
+        if (entity instanceof Tameable && !player.isSneaking() && ((Tameable) entity).isTamed()) {
             //Берет игрока для Audience
-            final Audience audience = adventure().player(e.getPlayer());
+            final Audience audience = adventure().player(player);
             //Берет моба на ПКМ
-            Tameable tm = (Tameable) e.getRightClicked();
+            Tameable tm = (Tameable) entity;
             //Мои заменители
             String placeholders = Format.tameablePlaceholders(Data.tameable_message, tm);
             //Форматирование цвета
