d03105910e5f6a6c3cf1e44c81000531075c8adc
en0tuk <kolobokovich123@gmail.com>
2022-11-20 22:37:43 +0600

    Remove ElytriumJavaCommons dependency

--- spigot/ru/enis/ehidetags/misc/configs/ConfigTOML.java
+++ spigot/ru/enis/ehidetags/misc/configs/ConfigTOML.java
@@ -1,29 +1,20 @@
 package ru.enis.ehidetags.misc.configs;
 
-import net.elytrium.java.commons.mc.serialization.Serializer;
-import net.elytrium.java.commons.mc.serialization.Serializers;
 import ru.enis.ehidetags.Core;
-import ru.enis.ehidetags.misc.logger.Log;
-
-import java.util.Objects;
 
 public class ConfigTOML {
 
    public ConfigTOML(Core plugin) {
       CustomTOML cfg = new CustomTOML("Main Config", plugin);
       Data.player_message = cfg.getField("Actionbar.message", "<gold>%name%",
-          """
-               Placeholders:
-               %name% - Player Name
-               %displayname% - Player Displayname""".indent(1));
-
+          " Placeholders:\n" +
+              " %name% - Player Name\n" +
+              " %displayname% - Player Displayname");
       Data.tameable_message = cfg.getField("Actionbar.tameable-message", "<white>%name%",
-          """
-               THIS CANNOT BE DISABLED
-               This fixes bug when tameable entity do not shows their name
-               Placeholders:
-               %name% - Tameable Entity Name""".indent(1));
-
+          " THIS CANNOT BE DISABLED\n" +
+              " This fixes bug when tameable entity do not shows their name\n" +
+              " Placeholders:\n" +
+              " %name% - Tameable Entity Name");
       Data.enabled = cfg.getField("Actionbar.enabled", true);
    }
 
--- spigot/ru/enis/ehidetags/misc/configs/Data.java
+++ spigot/ru/enis/ehidetags/misc/configs/Data.java
@@ -1,10 +1,7 @@
 package ru.enis.ehidetags.misc.configs;
 
-import net.elytrium.java.commons.mc.serialization.Serializer;
-
 public class Data {
     public static Boolean enabled;
     public static String player_message;
     public static String tameable_message;
-    public static Serializer serializer;
 }
